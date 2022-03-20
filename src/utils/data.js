4eed5d26e1d77a5b706d7604be2547e208c22f99
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2022-03-20 21:02:12 +0500

    Done with crud operation to add user in app

---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Huzaifa Ahmed",
+    username: "Huzaifa",
+  },
+  {
+    id: 2,
+    name: "Ali Imran",
+    username: "Ali",
+  },
+  {
+    id: 3,
+    name: "Hanzala Ahmed",
+    username: "Hanzala",
+  },
+  {
+    id: 4,
+    name: "Ameer Hamza",
+    username: "Hamza",
+  },
+  {
+    id: 5,
+    name: "Kamran Ghulam",
+    username: "Kamran",
+  },
+  {
+    id: 6,
+    name: "Arham Naeem",
+    username: "Arham",
+  },
+];
